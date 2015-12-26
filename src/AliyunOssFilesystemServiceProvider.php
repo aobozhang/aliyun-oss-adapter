3492fe14d54ca964141f5e691510a5a2ab255918
Aobo <aobozhang@gmail.com>
2015-12-26 16:22:48 +0800

    remove extra info

--- src/AliyunOssFilesystemServiceProvider.php
+++ src/AliyunOssFilesystemServiceProvider.php
@@ -10,13 +10,6 @@ use Illuminate\Support\ServiceProvider;
 
 class AliyunOssFilesystemServiceProvider extends ServiceProvider {
 
-    /**
-     * 服务提供者加是否延迟加载.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
     public function boot()
     {
         Storage::extend('oss', function($app, $config)
@@ -38,16 +31,6 @@ class AliyunOssFilesystemServiceProvider extends ServiceProvider {
     public function register()
     {
         //
-    }
-
-    /**
-     * 获取由提供者提供的服务.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return [];
     }
 
 }
